f6d361c77c2d3246457ffec2772d057d9c267643
keigo <kei.522.r@icloud.com>
2024-10-31 22:40:23 +0900

    サービス、ガイドライン、運営のページ更新

--- src/app/service/page.tsx
+++ src/app/service/page.tsx
@@ -1,7 +1,8 @@
 export default function Service() {
+  const headingStyle = "font-bold text-xl";
   return (
-    <div>
-      <h1 className=" text-center text-lg font-bold">リリカルとは</h1>
+    <div className="max-w-[840px] mx-auto">
+      <h1 className="text-center text-lg font-bold">リリカルとは</h1>
       <div className="flex flex-row justify-center space-x-4">
         <a
           href="http://localhost:3000/service"
@@ -16,28 +17,28 @@ export default function Service() {
           運営
         </a>
       </div>
-      <div className="border m-8">
-        <p className="font-bold">リリカルへようこそ</p>
-        <p>
-          リリカルは、ユーザーが音楽の楽曲情報・解釈・考察を投稿して、
-          ユーザー同士がそのコンテンツを楽しめるメディアプラットフォームです。
-        </p>
-      </div>
-      <div className="border m-8">
-        <p className="font-bold">リリカルが大切にしていること</p>
-        <p>
-          音楽をもっと楽しく。 音楽は耳で聴くだけじゃない！
-          歌詞・歌唱・映像・音源・振付などで彩られる、総合的な感覚体験である。
-          あなたの小さな気づきが、誰かにとっての新たな感動に繋がり、より素晴らしい音楽体験となる。
-          音楽と出会い、想いに共感して、好きを共有する場所、それがリリカル。
-        </p>
-      </div>
-      <div className="border m-8">
-        <p className="font-bold">リリカルの３つの特徴</p>
-        <p>01 アーティスト・楽曲の情報を追加・発見する</p>
-        <p>02 あなたしか知らないアーティスト・楽曲の魅力をシェアする</p>
-        <p>03 ユーザー同士で交流するSNSとして活用する</p>
-      </div>
+      {[
+        {
+          heading: "リリカルへようこそ",
+          description:
+            "リリカルは、ユーザーが音楽の楽曲情報・解釈・考察を投稿して、ユーザー同士がそのコンテンツを楽しめるメディアプラットフォームです。",
+        },
+        {
+          heading: "リリカルが大切にしていること",
+          description:
+            "音楽をもっと楽しく。 音楽は耳で聴くだけじゃない！歌詞・歌唱・映像・音源・振付などで彩られる、総合的な感覚体験である。あなたの小さな気づきが、誰かにとっての新たな感動に繋がり、より素晴らしい音楽体験となる。音楽と出会い、想いに共感して、好きを共有する場所、それがリリカル。",
+        },
+        {
+          heading: "リリカルが大切にしていること",
+          description:
+            "音楽をもっと楽しく。 音楽は耳で聴くだけじゃない！歌詞・歌唱・映像・音源・振付などで彩られる、総合的な感覚体験である。あなたの小さな気づきが、誰かにとっての新たな感動に繋がり、より素晴らしい音楽体験となる。音楽と出会い、想いに共感して、好きを共有する場所、それがリリカル。",
+        },
+      ].map((item, index) => (
+        <div key={index} className="border m-8 p-4">
+          <p className={headingStyle}>{item.heading}</p>
+          <p>{item.description}</p>
+        </div>
+      ))}
     </div>
   );
 }
