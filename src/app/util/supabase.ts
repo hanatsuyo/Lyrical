b68332d919e393a4e2e613e850f6696e244eca78
hanatsuyo <hanatsuyo.t5.14@gmail.com>
2024-11-04 22:58:55 +0900

    環境変数の修正

--- src/app/util/supabase.ts
+++ src/app/util/supabase.ts
@@ -10,14 +10,14 @@ const getSupabase = (
     },
   }
 ) => {
-  if (!process.env.NEXT_PUBLIC_SUPABASE_URL) {
+  if (!process.env.SUPABASE_URL) {
     throw new Error("NEXT_PUBLIC_SUPABASE_URL is not defined");
   }
   if (!process.env.SUPABASE_SERVICE_ROLE_KEY) {
     throw new Error("SUPABASE_SERVICE_ROLE_KEY is not defined");
   }
   const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.SUPABASE_URL,
     process.env.SUPABASE_SERVICE_ROLE_KEY,
     options
   );
