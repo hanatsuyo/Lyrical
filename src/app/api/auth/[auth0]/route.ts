e23bd66ac24dffbe460613dcc592283f93acc115
hanatsuyo <hanatsuyo.t5.14@gmail.com>
2024-11-04 21:25:32 +0900

    型エラー解消

--- src/app/api/auth/[auth0]/route.ts
+++ src/app/api/auth/[auth0]/route.ts
@@ -1,25 +1,3 @@
-import { handleAuth, handleCallback } from "@auth0/nextjs-auth0";
-import jwt from "jsonwebtoken";
+import { handleAuth } from "@auth0/nextjs-auth0";
 
 export const GET = handleAuth();
-
-const afterCallback = async (req: any, res: any, session: any) => {
-  const payload = {
-    userId: session.user.sub,
-    exp: Math.floor(Date.now() / 1000) + 60 * 60,
-  };
-
-  session.user.accessToken = jwt.sign(payload, process.env.SUPABASE_JWT_SECRET);
-
-  return session;
-};
-
-export default handleAuth({
-  async callback(req: any, res: any) {
-    try {
-      await handleCallback(req, res, { afterCallback });
-    } catch (error: any) {
-      res.status(error.status || 500).end(error.message);
-    }
-  },
-});
